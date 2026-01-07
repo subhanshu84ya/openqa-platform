4a23c0ba6de80fc9a8fd29ec3f03f7dc63c7060c
Ashish Jaishwal <itsmeashishkumar@gmail.com>
2026-01-07 15:09:54 +0000

    fix

--- openqa-backend/src/main/java/com/openqa/openqa_backend/controller/HealthController.java
+++ openqa-backend/src/main/java/com/openqa/openqa_backend/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class HealthController {
 
